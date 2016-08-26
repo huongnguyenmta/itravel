fbd891ab86c6763985f5e9c91afd358cea9cebad
cuongncFramgia <nguyen.chi.cuong@framgia.com>
2016-08-26 11:44:00 +0700

    cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  include CanCan::ControllerAdditions
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 
   private
   def configure_permitted_parameters
