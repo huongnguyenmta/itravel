ab28ef1c669ec9c116ec09a94da5f6f30eea7460
huongnguyenmta <huongnt0809@gmail.com>
2016-09-05 08:56:42 +0700

    admin-update-place

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -14,6 +14,19 @@ class Admin::PlacesController < Admin::BaseController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @place.update_attributes place_params
+      flash[:success] = flash_message "updated"
+      redirect_to admin_places_url
+    else
+      flash[:danger] = flash_message "not_updated"
+      render :edit
+    end
+  end
+
   private
   def place_params
     params.require(:place).permit :name, :address, :longitude, :latitude,
